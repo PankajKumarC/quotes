a285457830831693c9498cdd41db92e6cbc936af
Pankaj <pankajkumarc2019@srishakthi.ac.in>
2018-09-21 18:23:25 +0530

    navigations working

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import QuoteOfTheDay from './Components/QuoteOfTheDay/QuoteOfTheDay';
 import SignIn from './Components/SignIn/SignIn';
+import SignUp from './Components/SignUp/SignUp';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      route: 'Navigation'
+      route: 'home'
     }
   }
 
@@ -20,11 +21,15 @@ class App extends Component {
     return (
       <div className="App">
         <Navigation onRouteChange={this.onRouteChange}/> 
-        { this.state.route === 'SignIn' 
-          ? <SignIn onRouteChange={this.onRouteChange}/>
-        :<div>
-          <QuoteOfTheDay />
-        </div>
+        { this.state.route === 'home' 
+          ? <div>
+            <QuoteOfTheDay />
+          </div>
+          : (
+            this.state.route === 'SignIn' 
+            ? <SignIn onRouteChange={this.onRouteChange}/>
+            : <SignUp onRouteChange={this.onRouteChange}/>
+          )
         }
       </div>
     );
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -6,7 +6,7 @@ const Navigation = ({onRouteChange}) => {
 		<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
 			<h3 className="Heading">Random Quote Generator</h3>
 			<p onClick={() => onRouteChange('SignIn')} className='link pointer grow f3-ns pa3 link b green bg-animate hover-bg-white'>Sign In</p>
-			<p className='link pointer grow f3-ns pa3 link b green bg-animate hover-bg-white'>Sign Up</p>
+			<p onClick={() => onRouteChange('SignUp')} className='link pointer grow f3-ns pa3 link b green bg-animate hover-bg-white'>Sign Up</p>
 			<p className='link pointer grow f3-ns pa3 link b green bg-animate hover-bg-white'>About Us</p>
 		</nav>
 	);
